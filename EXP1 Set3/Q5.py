558c5fef13896c387e57719667e03744699f4112
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2023-01-31 22:11:26 +0530

    Update Q5.py

--- EXP1 Set3/Q5.py
+++ EXP1 Set3/Q5.py
@@ -1,9 +1,19 @@
-num_students = int(input("Enter the number of students: "))
-scores = []
-for i in range(num_students):
-    score = int(input(f"Enter score for student {i + 1}: "))
-    scores.append(score)
+def get_scores():
+    n = int(input("Enter the number of students: "))
+    scores = []
+    for i in range(n):
+        score = int(input("Enter the score for student {}: ".format(i+1)))
+        scores.append(score)
+    return scores
 
-scores.sort(reverse=True)
-print(f"The highest score is {scores[0]}")
-print(f"The second highest score is {scores[1]}")
+def highest_and_second_highest(scores):
+    scores.sort(reverse=True)
+    highest = scores[0]
+    second_highest = scores[1]
+    return highest, second_highest
+
+scores = get_scores()
+highest, second_highest = highest_and_second_highest(scores)
+
+print("The highest score is:", highest)
+print("The second highest score is:", second_highest)
