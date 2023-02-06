7adbaafa9ff3b6877c2fcf50d74162ed406e466f
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2023-02-06 22:23:06 +0530

    Add files via upload

---
+++ EXP2 Set3/Q5.py
@@ -0,0 +1,8 @@
+processor_info = {
+  "Processor": "AMD Ryzen 9",
+  "Number of cores": 16,
+  "Year of release": 2021
+}
+print("Processor:", processor_info["Processor"])
+print("Number of cores:", processor_info["Number of cores"])
+print("Year of release:", processor_info["Year of release"])
